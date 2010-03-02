7a241f5a576ef65547d263513745ded0ff9b85db
Andrew Ingram <andy@andrewingram.net>
2010-03-02 01:10:08 +0000

    added markdown container widget and demo page

--- src/js/widgets/core.js
+++ src/js/widgets/core.js
@@ -71,10 +71,10 @@ MONSTER.widgets.container = function(spec, my) {
 		select.append(option);
 	}		
 	
-	var handler = $('<div class="ui-datepicker-header ui-widget-header ui-helper-clearfix ui-corner-all" style="z-index: 9999; margin: 0 0 10px; padding: 2px;"><span class="add ui-datepicker-prev ui-corner-all"><span class="ui-icon ui-icon-plus"></span></span></div>');
+	var handler = $('<div class="ui-widget-header ui-helper-clearfix ui-corner-all" style="z-index: 9999; margin: 0 0 10px; padding: 2px;"><span class="add ui-corner-all"><span class="ui-icon ui-icon-plus"></span></span></div>');
 	
 	handler.prepend(select);
-	
+		
 	node.append(handler);		
 	
 	node.sortable({
@@ -82,7 +82,8 @@ MONSTER.widgets.container = function(spec, my) {
 		scroll: true,
 		cursorAt: 'bottom',
 		tolerance: 'pointer',
-		containment: 'parent'
+		containment: 'parent',
+		items: ':widget'
 	});
 
 	handler.children('span.add').click(function(){
@@ -114,7 +115,7 @@ MONSTER.widgets.block = function(spec, my) {
 		spec.editor.editor_for_node($(this), spec.data ? spec.data[i] : undefined);
 	});
 	
-	var handler = $('<div class="ui-datepicker-header ui-widget-header ui-helper-clearfix ui-corner-all" style="z-index: 9999; position: absolute; top: -2px; right: -2px; width: 32px; height: 16px; padding: 2px;"><span class="move ui-datepicker-prev ui-corner-all"><span class="ui-icon ui-icon-arrow-4">Move</span></span><span class="delete ui-datepicker-prev ui-corner-all"><span class="ui-icon ui-icon-trash">Remove</span></span></div>');		
+	var handler = $('<div class="ui-widget-header ui-helper-clearfix ui-corner-all" style="z-index: 9999; position: absolute; top: -2px; right: -2px; width: 32px; height: 16px; padding: 2px;"><span class="move ui-corner-all"><span class="ui-icon ui-icon-arrow-4">Move</span></span><span class="delete ui-corner-all"><span class="ui-icon ui-icon-trash">Remove</span></span></div>');		
 	node.prepend(handler);
 	
 	node.css({'position': 'relative'});
@@ -184,6 +185,7 @@ MONSTER.base.dialog_widget = function(spec, my) {
 	that.node.click(function(e){
 	
 		e.preventDefault();
+		e.stopPropagation();
 	
 		var container = $('<div id="monster-dialog-container"></div>');
 		
@@ -365,6 +367,106 @@ MONSTER.widgets.markdown = function(spec,my){
 				}
 			}
 		});
+	});
+	
+	that.get_data = function(){
+		return that.node.html();
+	};
+	that.render = function(){
+		that.node.html(that.data);
+	};
+			
+	return that;
+};
+
+MONSTER.widgets.markdown_container = function(spec,my){
+	var my = my || {};
+	var that = MONSTER.base.widget(spec,my);
+
+	var block_options = [];
+	var block_options_by_type = {};
+	
+	var node = spec.node;
+	
+	var build_options = function(i) {
+		
+		var $this = $(this);
+		
+		var html = $('<div />').append($this.clone()).remove().html();
+		var label = $this.attr('m:label');
+		
+		block_options_by_type[label] = html;
+		
+		block_options[i] = {
+			label: label,
+			html: html
+		};
+	};
+	
+	var container_nodes = function() {		
+		return $(this).attr('m:widget') !== 'container';
+	};
+	
+	node.widgets().filter(container_nodes).each(build_options);	
+		
+	node.empty();
+	
+	var md_node = $('<div m:widget="markdown"/>');
+	
+	MONSTER.widgets.markdown({
+		'node': md_node,
+		'data': 'placeholder',
+		'editor': spec.editor
+	});
+	
+	node.append(md_node);
+	
+	var select = $('<select>');
+			
+	for (var i=0; i < block_options.length; i++){
+
+		var temp = block_options[i];
+		
+		var option = $('<option>');
+		option.attr('value',i);
+		option.text(temp.label);
+
+		select.append(option);
+	}		
+	
+	var handler = $('<div class="ui-widget-header ui-helper-clearfix ui-corner-all" style="z-index: 9999; margin: 0 0 10px; padding: 2px;"><span class="add ui-corner-all"><span class="ui-icon ui-icon-plus"></span></span></div>');
+	
+	handler.prepend(select);
+		
+	node.append(handler);		
+	
+	md_node.sortable({
+		handle: 'span.move',
+		scroll: true,
+		cursorAt: 'top left',
+		tolerance: 'intersect',
+		cancel: 'p',
+		helper: 'clone',
+		//placeholder: 'ui-state-highlight'
+		//forcePlaceholderSize: true,
+		//cursor: 'crosshair',
+	});
+
+	handler.children('span.add').click(function(){
+		var new_node = $(block_options[select.get(0).value]['html']);
+		new_node.hide();
+		md_node.prepend(new_node);
+		
+		MONSTER.widgets.block({
+			'node': new_node,
+			'data': undefined,
+			'editor': spec.editor
+		});		
+		
+		md_node.sortable('refresh');	
+		
+		new_node.fadeIn(400);
+		return false;
 	});
 	
 	that.get_data = function(){
